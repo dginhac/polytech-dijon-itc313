c7e627417dab5464eac837855132483b6c9abb35
d0m G <dominique.ginhac@u-bourgogne.fr>
2025-11-12 11:07:13 +0100

    addon on maintanability on lecture 02 and update code samples

---
+++ samples/02-inheritance/01b-Ebook/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "book.h"
+#include "ebook.h"
+
+int main() {
+    Author author("George", "Orwell");
+    Book book("1984", author);
+    Ebook ebook("1984", author, "http://example.com/1984");
+
+    std::cout << "Author: " << ebook.author().firstname() << " " << ebook.author().lastname() << std::endl;
+    std::cout << "Book: " << book.description() << std::endl;
+    std::cout << "Ebook: "<< ebook.description() << std::endl;
+    ebook.download();
+    return 0;
+}
